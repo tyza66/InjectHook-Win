98d768e87d87476de43ecc4c4e00a3e9f298d0fa
洮羱芝闇 <shun_@outlook.com>
2025-01-07 13:21:56 +0800

    C# OK

---
+++ csHook/aHook/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aHook
+{
+    public class Class1
+    {
+        public void ShowMessage()
+        {       // 右键引用->添加引用->浏览->选择System.Windows.Forms.dll
+            MessageBox.Show("OK","C# OK Title");
+        }
+    }
+}
+// 这样还不能直接注入运行，因为没有入口函数，所以需要一个C++入口函数
---
+++ csHook/cHeader/dllmain.cpp
@@ -0,0 +1,32 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+// 右键属性添加公关语言运行时支持
+// .NET版本填写 4.7.2
+// 语言中符合模式改成否
+using namespace aHook;
+
+void Test(){
+    Class1 ^class1 = gcnew Class1();
+    class1->ShowMessage();
+}
+// 引用 添加 CSHOOK
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        Test();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
